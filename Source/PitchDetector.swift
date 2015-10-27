d6ef3d9b962210bf66407c1734c25a38dd3fc87f
Vadym Markov <markov.vadym@gmail.com>
2015-10-28 00:53:36 +0100

    Init private vars

--- Source/PitchDetector.swift
+++ Source/PitchDetector.swift
@@ -14,15 +14,20 @@ public class PitchDetector {
   }
 
   public weak var delegate: PitchDetectorDelegate?
-
   public var isActive = false
   public var highBoundFrequency: Int = 0
   public var lowBoundFrequency: Int = 0
-  public var sampleRate: Double
+  public var sampleRate: Float
+
+  private var bufferLength: Int
+  private var hanningWindow: UnsafeMutablePointer<Float>
+  private var result: UnsafeMutablePointer<Float>
+  private var buffer: UnsafeMutablePointer<Int16>
+  private var samplesInBuffer: Int
 
   // MARK: - Initialization
 
-  public init(sampleRate: Double,
+  public init(sampleRate: Float,
     lowBoundFrequency: Int = Defaults.lowBoundFrequency,
     highBoundFrequency: Int = Defaults.highBoundFrequency,
     delegate: PitchDetectorDelegate? = nil) {
@@ -30,6 +35,14 @@ public class PitchDetector {
       self.lowBoundFrequency = lowBoundFrequency
       self.highBoundFrequency = highBoundFrequency
       self.delegate = delegate
+
+      bufferLength = Int(sampleRate) / lowBoundFrequency
+      hanningWindow = UnsafeMutablePointer<Float>.alloc(bufferLength)
+      result = UnsafeMutablePointer<Float>.alloc(bufferLength)
+      buffer = UnsafeMutablePointer<Int16>.alloc(512)
+      samplesInBuffer = 0
+
+      vDSP_hann_window(hanningWindow, vDSP_Length(bufferLength), Int32(vDSP_HANN_NORM))
   }
 
   public func addSamples(samples: Int16, inNumberFrames frames: Int) {
