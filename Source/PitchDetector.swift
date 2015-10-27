629a5ec8b38479896b6995260fc6100edd2aca6d
Vadym Markov <markov.vadym@gmail.com>
2015-10-28 00:29:00 +0100

    Init with default values

--- Source/PitchDetector.swift
+++ Source/PitchDetector.swift
@@ -8,5 +8,31 @@ public protocol PitchDetectorDelegate: class {
 
 public class PitchDetector {
 
+  public struct Defaults {
+    static let lowBoundFrequency = 40
+    static let highBoundFrequency = 4500
+  }
+
   public weak var delegate: PitchDetectorDelegate?
+
+  public var isActive = false
+  public var highBoundFrequency: Int = 0
+  public var lowBoundFrequency: Int = 0
+  public var sampleRate: Double
+
+  // MARK: - Initialization
+
+  public init(sampleRate: Double,
+    lowBoundFrequency: Int = Defaults.lowBoundFrequency,
+    highBoundFrequency: Int = Defaults.highBoundFrequency,
+    delegate: PitchDetectorDelegate? = nil) {
+      self.sampleRate = sampleRate
+      self.lowBoundFrequency = lowBoundFrequency
+      self.highBoundFrequency = highBoundFrequency
+      self.delegate = delegate
+  }
+
+  public func addSamples(samples: Int16, inNumberFrames frames: Int) {
+
+  }
 }
