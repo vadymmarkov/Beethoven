93a4a919b6f950276c0345c564cbcc54e542c77f
Vadym Markov <markov.vadym@gmail.com>
2015-10-29 23:05:43 +0100

    Add audio processor

---
+++ Source/AudioProcessor.swift
@@ -0,0 +1,38 @@
+import AVFoundation
+
+public protocol AudioProcessorDelegate: class {
+  func audioProcessorDidReceiveSamples(samples: UnsafeMutablePointer<Int16>, framesCount: Int)
+}
+
+public class AudioProcessor {
+
+  private let audioEngine = AVAudioEngine()
+  public weak var delegate: AudioProcessorDelegate?
+
+  public func start() {
+    guard let inputNode = audioEngine.inputNode else { return }
+
+    let bus = 0
+
+    inputNode.installTapOnBus(bus, bufferSize: 2048, format:inputNode.inputFormatForBus(bus)) {
+      (buffer: AVAudioPCMBuffer!, time: AVAudioTime!) -> Void in
+      dispatch_async(dispatch_get_main_queue()) {
+        self.delegate?.audioProcessorDidReceiveSamples(buffer.int16ChannelData[0], framesCount: Int(buffer.frameLength))
+      }
+    }
+
+    audioEngine.prepare()
+    
+    do {
+      try audioEngine.start()
+      print("Star")
+    } catch {
+      print("Error")
+    }
+  }
+
+  public func stop() {
+    print("Stop")
+    audioEngine.stop()
+  }
+}
