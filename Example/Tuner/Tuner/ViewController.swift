9be70a0487976914c158721d4d860daab48eed14
Vadym Markov <markov.vadym@gmail.com>
2015-11-03 22:11:44 +0100

    Create demo project

---
+++ Example/Tuner/Tuner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tuner
+//
+//  Created by Vadym Markov on 03/11/15.
+//  Copyright © 2015 Vadym Markov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
