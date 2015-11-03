9a17ecf6a1ba735fd9fff51e8e7788897255606f
Vadym Markov <markov.vadym@gmail.com>
2015-11-03 22:31:58 +0100

    Use engine

--- Example/Tuner/Tuner/ViewController.swift
+++ Example/Tuner/Tuner/ViewController.swift
@@ -1,16 +1,32 @@
 import UIKit
+import PitchAssistant
 
 class ViewController: UIViewController {
+
+  lazy var pitchEngine: PitchEngine = { [unowned self] in
+    let pitchEngine = PitchEngine(
+      bufferSize: 2048,
+      delegate: self
+    )
+
+    return pitchEngine
+    }()
+
+  // MARK: - View Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
     title = "Tuner"
   }
+}
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+// MARK: - PitchEngineDelegate
+
+extension ViewController: PitchEngineDelegate {
+
+  func pitchEngineDidRecieveFrequency(pitchEngine: PitchEngine, frequency: Float) {
+
   }
 }
 
