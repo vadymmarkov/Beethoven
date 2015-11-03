ebd7212bd7aec068e1e88ba6eeb9e7b6e55311ad
Vadym Markov <markov.vadym@gmail.com>
2015-11-03 22:28:15 +0100

    Present controller

--- Example/Tuner/Tuner/ViewController.swift
+++ Example/Tuner/Tuner/ViewController.swift
@@ -1,25 +1,16 @@
-//
-//  ViewController.swift
-//  Tuner
-//
-//  Created by Vadym Markov on 03/11/15.
-//  Copyright © 2015 Vadym Markov. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    title = "Tuner"
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
