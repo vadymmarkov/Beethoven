91e44f7e73d9850cac4dd3f1fbf51442c68618e5
Vadym Markov <markov.vadym@gmail.com>
2015-11-05 02:52:55 +0100

    Refactor code

--- Source/Pitch.swift
+++ Source/Pitch.swift
@@ -20,10 +20,8 @@ public struct Pitch {
   public init(frequency: Float) {
     let n = Int(round(12 * log2(frequency / Base.frequency)))
     let index = n < 0 ? 12 - abs(n) % 12 : n % 12
+
     self.note = Note.values[index]
-
-
-
     self.octave = n < 0
       ? Base.octave - (abs(n) + 2) / 12
       : Base.octave + (n + 9) / 12
