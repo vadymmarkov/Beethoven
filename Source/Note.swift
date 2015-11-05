271a9038a9e7cf05060dbf882ee7783036415a8a
Vadym Markov <markov.vadym@gmail.com>
2015-11-05 01:13:33 +0100

    Add Note enum

---
+++ Source/Note.swift
@@ -0,0 +1,19 @@
+public enum Note: String {
+  case A = "A"
+  case ASharp = "A#"
+  case B = "B"
+  case C = "C"
+  case CSharp = "C#"
+  case D = "D"
+  case DSharp = "D#"
+  case E = "E"
+  case F = "F"
+  case FSharp = "F#"
+  case G = "G"
+  case GSharp = "G#"
+
+  public static var values = [
+    A, ASharp, B, C, CSharp, D,
+    DSharp, E, F, FSharp, G, GSharp
+  ]
+}
---
+++ Source/Pitch.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public struct Pitch {
+
+
+}
