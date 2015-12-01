bdc52459a21817d298cda65c4018459945a0af5f
Vadym Markov <markov.vadym@gmail.com>
2015-12-02 00:36:48 +0100

    Max value estimator

--- Source/Estimation/Strategies/MaxValueEstimator.swift
+++ Source/Estimation/Strategies/MaxValueEstimator.swift
@@ -1,1 +1,16 @@
 import Foundation
+
+public class MaxValueEstimator: EstimationAware {
+
+  public func estimateLocation(transformResult: TransformResult, sampleRate: Float) -> Int {
+    let buffer = transformResult.buffer
+
+    guard let maxElement = buffer.maxElement(),
+      maxIndex = buffer.indexOf(maxElement) else {
+        return 0
+    }
+
+    return maxIndex
+  }
+}
+
--- Source/Estimation/Strategies/QuadradicEstimator.swift
+++ Source/Estimation/Strategies/QuadradicEstimator.swift
@@ -3,13 +3,14 @@ import Foundation
 public class QuadradicEstimator: EstimationAware {
 
   public func estimateLocation(transformResult: TransformResult, sampleRate: Float) -> Int {
-    var buffer = transformResult.buffer
+    let buffer = transformResult.buffer
+
     guard let maxElement = buffer.maxElement(),
       maxIndex = buffer.indexOf(maxElement) else {
         return 0
     }
 
-    let y2  =  abs(buffer[maxIndex])
+    let y2 = abs(buffer[maxIndex])
     let y1 = maxIndex == 0 ? y2 : abs(buffer[maxIndex - 1])
     let y3 = maxIndex == buffer.count - 1 ? y2 : abs(buffer[maxIndex + 1])
     let d = (y3 - y1) / (2 * (2 * y2 - y1 - y3))
