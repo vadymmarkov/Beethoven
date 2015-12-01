dad0bb1d89bb3ad4b6a3ba37243592adc44df07f
Vadym Markov <markov.vadym@gmail.com>
2015-12-02 00:35:11 +0100

    Barycentric estimator

--- Source/Estimation/Strategies/BarycentricEstimator.swift
+++ Source/Estimation/Strategies/BarycentricEstimator.swift
@@ -1,1 +1,22 @@
 import Foundation
+
+public class BarycentricEstimator: EstimationAware {
+
+  public func estimateLocation(transformResult: TransformResult, sampleRate: Float) -> Int {
+    var buffer = transformResult.buffer
+    guard let maxElement = buffer.maxElement(),
+      maxIndex = buffer.indexOf(maxElement) else {
+        return 0
+    }
+
+    let y2  =  abs(buffer[maxIndex])
+    let y1 = maxIndex == 0 ? y2 : abs(buffer[maxIndex - 1])
+    let y3 = maxIndex == buffer.count - 1 ? y2 : abs(buffer[maxIndex + 1])
+    let d = (y3 - y1) / (y1 + y2 + y3)
+    let location = maxIndex + Int(round(d))
+
+    return location >= 0 && location < buffer.count
+      ? location
+      : maxIndex
+  }
+}
