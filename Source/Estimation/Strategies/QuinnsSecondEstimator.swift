68e822c6aa02ed39e46b919ee74acb7f3f22930b
Vadym Markov <markov.vadym@gmail.com>
2015-12-02 01:00:16 +0100

    Quinns second estimator

--- Source/Estimation/Strategies/QuinnsSecondEstimator.swift
+++ Source/Estimation/Strategies/QuinnsSecondEstimator.swift
@@ -6,38 +6,36 @@ public class QuinnsSecondEstimator: EstimationAware {
     let buffer = transformResult.buffer
 
     guard let maxElement = buffer.maxElement(),
-      k = buffer.indexOf(maxElement) else {
+      maxIndex = buffer.indexOf(maxElement) else {
         return 0
     }
 
     guard let complexBuffer = transformResult.complexBuffer else {
-      return k
+      return maxIndex
     }
 
     let realp = complexBuffer.realp
     let imagp = complexBuffer.imagp
 
-    let ap1 = realp[k + 1] * realp[k] + imagp[k+1] * imagp[k]
-    let ap2 = realp[k] * realp[k] + imagp[k] * imagp[k]
+    let prevIndex = maxIndex == 0 ? maxIndex : maxIndex - 1
+    let nextIndex = maxIndex == buffer.count - 1 ? maxIndex : maxIndex + 1
+    let divider = pow(realp[maxIndex], 2.0) + pow(imagp[maxIndex], 2.0)
 
-    let ap = ap1 / ap2
-    let dp = -ap / (1 - ap)
-
-    let am1 = realp[k - 1] * realp[k] + imagp[k - 1] * imagp[k]
-    let am2 = realp[k] * realp[k] + imagp[k] * imagp[k]
-
-    let am = am1  / am2
-    let dm = am / (1 - am)
+    let ap = (realp[nextIndex] * realp[maxIndex] + imagp[nextIndex] * imagp[maxIndex]) / divider
+    let dp = -ap  / (1.0 - ap)
+    let am = (realp[prevIndex] * realp[maxIndex] + imagp[prevIndex] * imagp[maxIndex]) / divider
+    let dm = am / (1.0 - am)
     let d = (dp + dm) / 2 + tau(dp * dp) - tau(dm * dm)
-    let r = k + Int(round(d))
+    let location = maxIndex + Int(round(d))
 
-    return r
+    return location >= 0 && location < buffer.count
+      ? location
+      : maxIndex
   }
 
-  private func tau(x: Float) -> Float {
-    let p1 = log(3 * x * x + 6 * x + 1)
-    let p2 = x + 1 - sqrt(2/3)
-    let p3 = log(p2) /  (x + 1 + sqrt(2/3))
-    return 1/4 * p1 - sqrt(6)/24 * p3
+  func tau(x: Float) -> Float {
+    let p1 = log(3 * pow(x, 2.0) + 6 * x + 1)
+    let p2 = log((x + 1 - sqrt(2/3)) / (x + 1 + sqrt(2/3)))
+    return 1/4 * p1 - sqrt(6)/24 * p2
   }
 }
