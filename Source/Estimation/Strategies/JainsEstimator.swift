c746ed36eb9cf8bf35e4a738529ede23b727285e
Vadym Markov <markov.vadym@gmail.com>
2015-12-02 01:04:35 +0100

    Jains estimator

--- Source/Estimation/Strategies/JainsEstimator.swift
+++ Source/Estimation/Strategies/JainsEstimator.swift
@@ -1,1 +1,32 @@
 import Foundation
+
+public class JainsEstimator: EstimationAware {
+
+  public func estimateLocation(transformResult: TransformResult, sampleRate: Float) -> Int {
+    let buffer = transformResult.buffer
+
+    guard let maxElement = buffer.maxElement(),
+      maxIndex = buffer.indexOf(maxElement) else {
+        return 0
+    }
+
+    let y2 = abs(buffer[maxIndex])
+    let y1 = maxIndex == 0 ? y2 : abs(buffer[maxIndex - 1])
+    let y3 = maxIndex == buffer.count - 1 ? y2 : abs(buffer[maxIndex + 1])
+    let location: Int
+
+    if y1 > y3 {
+      let a = y2 / y1
+      let d = a / (1 + a)
+      location = maxIndex - 1 + Int(round(d))
+    } else {
+      let a = y3 / y2
+      let d = a / (1 + a)
+      location = maxIndex + Int(round(d))
+    }
+
+    return location >= 0 && location < buffer.count
+      ? location
+      : maxIndex
+  }
+}
--- Source/Estimation/Strategies/QuinnsFirstEstimator.swift
+++ Source/Estimation/Strategies/QuinnsFirstEstimator.swift
@@ -35,20 +35,7 @@ public class QuinnsFirstEstimator: EstimationAware {
 }
 
 
-/*
-let y1 = abs(normalizedMagnitudes[k-1])
-let y2 = abs(normalizedMagnitudes[k])
-let y3 = abs(normalizedMagnitudes[k+1])
-if y1 > y3 {
-let a = y2  /  y1
-let d = a  /  (1 + a)
-r = k - 1 + Int(round(d))
-} else {
-let a = y3  /  y2
-let d = a  /  (1 + a)
-r = k + Int(round(d))
-}
-//let r  =  k + Int(d)*/
+
 
 
 
