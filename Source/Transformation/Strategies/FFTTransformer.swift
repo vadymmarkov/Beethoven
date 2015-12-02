cfafbc61f5550f6a74ce48ff2a883eb3356b0e0a
Vadym Markov <markov.vadym@gmail.com>
2015-12-02 23:42:04 +0100

    Add hanning window to FFT

--- Source/Transformation/Strategies/FFTTransformer.swift
+++ Source/Transformation/Strategies/FFTTransformer.swift
@@ -14,11 +14,18 @@ public class FFTTransformer: TransformAware {
     var imagp = [Float](count: inputCount, repeatedValue: 0)
     var output = DSPSplitComplex(realp: &realp, imagp: &imagp)
 
-    // TODO: Improve this
-    vDSP_hann_window(&realp, vDSP_Length(inputCount), Int32(vDSP_HANN_NORM))
+    let windowSize = Int(frameCount)
+    var transferBuffer = [Float](count: windowSize, repeatedValue: 0)
+    var window = [Float](count: windowSize, repeatedValue: 0)
+    memset(&window, 0, sizeof(Float) * windowSize)
+    vDSP_hann_window(&window, vDSP_Length(windowSize), Int32(vDSP_HANN_NORM))
+    vDSP_vmul(buffer.floatChannelData.memory, 1, window, 1, &transferBuffer, 1, vDSP_Length(windowSize))
 
-    vDSP_ctoz(UnsafePointer<DSPComplex>(buffer.floatChannelData.memory), 2,
+    vDSP_ctoz(UnsafePointer<DSPComplex>(transferBuffer), 2,
       &output, 1, vDSP_Length(inputCount))
+
+//    vDSP_ctoz(UnsafePointer<DSPComplex>(buffer.floatChannelData.memory), 2,
+//      &output, 1, vDSP_Length(inputCount))
     vDSP_fft_zrip(fftSetup, &output, 1, log2n, FFTDirection(FFT_FORWARD))
 
     var magnitudes = [Float](count:inputCount, repeatedValue:0.0)
