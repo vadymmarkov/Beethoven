5461adc5451dae70b40c3e478450f21146ee582a
Vadym Markov <markov.vadym@gmail.com>
2015-12-02 23:46:45 +0100

    Move convert to extension

--- Source/Transformation/Strategies/SimpleTransformer.swift
+++ Source/Transformation/Strategies/SimpleTransformer.swift
@@ -4,15 +4,8 @@ public class SimpleTransformer: TransformAware {
 
   public func transformBuffer(buffer: AVAudioPCMBuffer) -> Buffer {
     let pointer = UnsafePointer<Float>(buffer.floatChannelData)
-    let elements = convert(Int(buffer.frameLength), data: pointer)
+    let elements = Array.fromUnsafePointer(pointer, count: Int(buffer.frameLength))
 
     return Buffer(elements: elements)
-  }
-
-  // MARK: - Helpers
-
-  func convert<T>(count: Int, data: UnsafePointer<T>) -> [T] {
-    let buffer = UnsafeBufferPointer(start: data, count: count);
-    return Array(buffer)
   }
 }
