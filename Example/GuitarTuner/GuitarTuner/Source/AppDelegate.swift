d8dba50a9d6f39611a3ab990111fa3599174b5d6
Vadym Markov <markov.vadym@gmail.com>
2016-01-02 19:13:12 +0100

    Style demo

--- Example/GuitarTuner/GuitarTuner/Source/AppDelegate.swift
+++ Example/GuitarTuner/GuitarTuner/Source/AppDelegate.swift
@@ -12,24 +12,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
   lazy var viewController: ViewController = {
-    return ViewController()
+    let controller = ViewController()
+    return controller
     }()
 
-  func application(
-    application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-      window = UIWindow(frame: UIScreen.mainScreen().bounds)
-      window?.rootViewController = navigationController
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    window = UIWindow(frame: UIScreen.mainScreen().bounds)
+    window?.rootViewController = navigationController
 
-      applyStyles()
+    applyStyles()
 
-      window?.makeKeyAndVisible()
+    window?.makeKeyAndVisible()
 
-      return true
+    return true
   }
 
   func applyStyles() {
     let navigationBar = UINavigationBar.appearance()
+    navigationBar.barStyle = .Black
     navigationBar.barTintColor = UIColor.hex("111011")
     navigationBar.translucent = false
     navigationBar.shadowImage = UIImage()
--- Example/GuitarTuner/GuitarTuner/Source/ViewController.swift
+++ Example/GuitarTuner/GuitarTuner/Source/ViewController.swift
@@ -8,76 +8,55 @@ class ViewController: UIViewController {
 
   lazy var noteLabel: UILabel = {
     let label = UILabel()
-    label.text = "A4"
-    label.font = UIFont.boldSystemFontOfSize(70)
+    label.text = "--"
+    label.font = UIFont.boldSystemFontOfSize(65)
     label.textColor = UIColor.hex("DCD9DB")
     label.textAlignment = .Center
     label.numberOfLines = 0
     label.sizeToFit()
 
     return label
-  }()
+    }()
 
-  lazy var leftOffsetLabel: UILabel = {
+  lazy var offsetLabel: UILabel = { [unowned self] in
     let label = UILabel()
-    label.text = "+50%"
-    label.font = UIFont.systemFontOfSize(24)
+    label.font = UIFont.systemFontOfSize(28)
     label.textColor = UIColor.whiteColor()
     label.textAlignment = .Center
     label.numberOfLines = 0
     label.sizeToFit()
 
     return label
-  }()
-
-  lazy var rightOffsetLabel: UILabel = {
-    let label = UILabel()
-    label.text = "-50%"
-    label.font = UIFont.systemFontOfSize(24)
-    label.textColor = UIColor.whiteColor()
-    label.textAlignment = .Center
-    label.numberOfLines = 0
-    label.sizeToFit()
-
-    return label
-  }()
+    }()
 
   lazy var actionButton: UIButton = { [unowned self] in
     let button = UIButton(type: .System)
-    button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
     button.layer.cornerRadius = 20
-    button.backgroundColor = UIColor.hex("E13C6C")
+    button.backgroundColor = UIColor.hex("3DAFAE")
+    button.titleLabel?.font = UIFont.systemFontOfSize(20)
+    button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
 
     button.addTarget(self, action: "actionButtonDidPress:",
       forControlEvents: .TouchUpInside)
     button.setTitle("Start".uppercaseString, forState: .Normal)
-    button.titleLabel?.font = UIFont(name: "HelveticaNeue", size: 18)!
 
     return button
-  }()
+    }()
 
-  lazy var tuner: PitchEngine = { [unowned self] in
-    let pitchEngine = PitchEngine(
-      delegate: self
-    )
-
+  lazy var pitchEngine: PitchEngine = { [unowned self] in
+    let pitchEngine = PitchEngine(delegate: self)
     return pitchEngine
-  }()
+    }()
 
   // MARK: - View Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    title = "Pitchy".uppercaseString
-    view.backgroundColor = UIColor.hex("181717")
+    title = "Tuner".uppercaseString
+    view.backgroundColor = UIColor.hex("111011")
 
-
-    //let gradientLayer = [UIColor.hex("224AAA"), UIColor.hex("3DAFAE")].gradient()
-    //gradientLayer.frame = view.frame
-    //view.layer.insertSublayer(gradientLayer, atIndex: 0)
-
-    [noteLabel, actionButton, leftOffsetLabel, rightOffsetLabel].forEach {
+    [noteLabel, actionButton, offsetLabel].forEach {
       view.addSubview($0)
     }
 
@@ -87,11 +66,18 @@ class ViewController: UIViewController {
   // MARK: - Action methods
 
   func actionButtonDidPress(button: UIButton) {
+    let text = pitchEngine.active
+      ? NSLocalizedString("Start", comment: "").uppercaseString
+      : NSLocalizedString("Stop", comment: "").uppercaseString
+
+    button.setTitle(text, forState: .Normal)
+    button.backgroundColor = pitchEngine.active
+      ? UIColor.hex("3DAFAE")
+      : UIColor.hex("E13C6C")
+
     noteLabel.text = "--"
-    tuner.active ? tuner.stop() : tuner.start()
-    tuner.active
-      ? button.setTitle("Stop".uppercaseString, forState: .Normal)
-      : button.setTitle("Start".uppercaseString, forState: .Normal)
+    pitchEngine.active ? pitchEngine.stop() : pitchEngine.start()
+    offsetLabel.hidden = !pitchEngine.active
   }
 
   // MARK: - Constrains
@@ -99,33 +85,43 @@ class ViewController: UIViewController {
   func setupLayout() {
     let totalSize = UIScreen.mainScreen().bounds
 
-    constrain(actionButton, noteLabel) { actionButton, noteLabel in
+    constrain(actionButton, noteLabel, offsetLabel) {
+      actionButton, noteLabel, offsetLabel in
+
       let superview = actionButton.superview!
 
-      actionButton.top == superview.top + (totalSize.height - 100) / 2
+      actionButton.top == superview.top + (totalSize.height - 30) / 2
       actionButton.centerX == superview.centerX
       actionButton.width == 280
       actionButton.height == 50
 
-      noteLabel.top == actionButton.top - 180
-      noteLabel.centerX == superview.centerX
-      noteLabel.width == 100
+      offsetLabel.bottom == actionButton.top - 60
+      offsetLabel.leading == superview.leading
+      offsetLabel.trailing == superview.trailing
+      offsetLabel.height == 80
+
+      noteLabel.bottom == offsetLabel.top - 20
+      noteLabel.leading == superview.leading
+      noteLabel.trailing == superview.trailing
       noteLabel.height == 80
     }
+  }
 
-    constrain(noteLabel, leftOffsetLabel, rightOffsetLabel) {
-      noteLabel, leftOffsetLabel, rightOffsetLabel in
+  // MARK: - UI
 
-      leftOffsetLabel.top == noteLabel.top
-      leftOffsetLabel.right == noteLabel.left - 25
-      leftOffsetLabel.width == 70
-      leftOffsetLabel.height == 80
+  func offsetColor(offsetPercentage: Double) -> UIColor {
+    let color: UIColor
 
-      rightOffsetLabel.top == noteLabel.top
-      rightOffsetLabel.left == noteLabel.right + 25
-      rightOffsetLabel.width == 70
-      rightOffsetLabel.height == 80
+    switch abs(offsetPercentage) {
+    case 0...5:
+      color = UIColor.hex("3DAFAE")
+    case 6...25:
+      color = UIColor.hex("FDFFB1")
+    default:
+      color = UIColor.hex("E13C6C")
     }
+
+    return color
   }
 }
 
@@ -137,9 +133,18 @@ extension ViewController: PitchEngineDelegate {
     noteLabel.text = pitch.note.string
 
     let offsetPercentage = pitch.closestOffset.percentage
-    let label = offsetPercentage > 0 ? rightOffsetLabel : leftOffsetLabel
+    let absOffsetPercentage = abs(offsetPercentage)
 
-    label.text = "\(offsetPercentage)%"
+    guard absOffsetPercentage > 1.0 else {
+      return
+    }
+
+    let prefix = offsetPercentage > 0 ? "+" : "-"
+    let color = offsetColor(offsetPercentage)
+
+    offsetLabel.text = "\(prefix)" + String(format:"%.2f", absOffsetPercentage) + "%"
+    offsetLabel.textColor = color
+    offsetLabel.hidden = false
   }
 
   func pitchEngineDidRecieveError(pitchEngine: PitchEngine, error: ErrorType) {
