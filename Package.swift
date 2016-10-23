500d509d7e581b136b8789d029ea97fdd7c586c4
Vadym Markov <markov.vadym@gmail.com>
2016-10-23 18:50:05 +0200

    Refactor project structure

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "Beethoven",
   dependencies: [
-    .Package(url: "https://github.com/vadymmarkov/Pitchy.git", majorVersion: 1),
+    .Package(url: "https://github.com/vadymmarkov/Pitchy.git", majorVersion: 2),
   ]
 )
